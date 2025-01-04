d2b61db04e7d588b0d720a7e197b22fab81bc1ca
Prachi <143865589+Prachichi7@users.noreply.github.com>
2025-01-04 19:44:24 +0530

    Add files via upload
    This sentiment analyser in Python uses a simple python library that offers API access to different NLP tasks such as sentiment analysis, spelling correction, which in TextBlob

---
+++ analyzer.py
@@ -0,0 +1,17 @@
+from textblob import TextBlob
+
+text= TextBlob(input("Enter the text to analyzer the sentiment: "))
+
+#The text.detect_language()
+print(text.sentiment)
+Sentiment= text.sentiment.polarity
+
+match Sentiment:
+    case x if x > 0:
+        print("Positive")
+    case x if x == 0:
+        print("Neutral")
+    case x if x < 0:
+        print("negative")
+    case _:
+        print("Unknown sentiment!")
